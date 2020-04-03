822cfa6b84d6fe42e09b61d5c4b882e947a9c8ad
bm.abir <bm.abir@chowagiken.co.jp>
2020-04-03 06:44:45 +0600

    bottom model is shown on add button press

--- lib/ui/reliefMapPage.dart
+++ lib/ui/reliefMapPage.dart
@@ -15,8 +15,18 @@ class _ReliefMapPageState extends State<ReliefMapPage>{
   Position _currentPosition;
 
   GoogleMapController mapController;
-  // final LatLng _center = const LatLng(23.7805733,90.2792399);
+  static const LatLng _center = const LatLng(23.7805733,90.2792399);
 
+  final Set<Marker> _markers = {
+                  //Marker for current Location
+                  // Marker(
+                  //   markerId: MarkerId("marker"),
+                  //   position: LatLng(_currentPosition.latitude, _currentPosition.longitude),
+                  //   infoWindow: InfoWindow(title: 'Current Location'),
+                  //   icon: BitmapDescriptor.defaultMarkerWithHue(BitmapDescriptor.hueBlue)
+                  // )  
+              };
+  LatLng _lastMapPosition = _center;
   void _onMapCreated(GoogleMapController controller) {
     mapController = controller;
   }
@@ -43,15 +53,8 @@ class _ReliefMapPageState extends State<ReliefMapPage>{
                 target: LatLng(_currentPosition.latitude,_currentPosition.longitude),
                 zoom: 11.0,
               ),
-              markers: {
-                  //Marker for current Location
-                  Marker(
-                    markerId: MarkerId("marker"),
-                    position: LatLng(_currentPosition.latitude, _currentPosition.longitude),
-                    infoWindow: InfoWindow(title: 'Current Location'),
-                    icon: BitmapDescriptor.defaultMarkerWithHue(BitmapDescriptor.hueBlue)
-                  )  
-              },
+              markers: _markers,
+              onCameraMove: _onCameraMove,
             ),
             // Ref: https://api.flutter.dev/flutter/material/FloatingActionButton-class.html#material.FloatingActionButton.2
             Padding(
@@ -60,7 +63,10 @@ class _ReliefMapPageState extends State<ReliefMapPage>{
                 alignment: Alignment.bottomRight,
                 child:FloatingActionButton.extended(
                   onPressed: () {
-                    // Add your onPressed code here!
+                    _showQuantityModal(context);
+                    // showModalBottomSheet(context: context, builder: (context)=> Container(
+                    //       color: Colors.red,
+                    //     ));
                   },
                   label: Text('New Relief'),
                   icon: Icon(Icons.add),
@@ -73,6 +79,66 @@ class _ReliefMapPageState extends State<ReliefMapPage>{
         
         
       );
+  }
+
+  // Ref https://medium.com/@rajesh.muthyala/flutter-with-google-maps-and-google-place-85ccee3f0371
+  void _onCameraMove(CameraPosition position) {
+  _lastMapPosition = position.target;
+  }
+  void _onAddMarkerButtonPressed() {
+    setState(() {
+      _markers.add(
+        Marker(
+          // This marker id can be anything that uniquely identifies each marker.
+          markerId: MarkerId(_lastMapPosition.toString()),
+          position: _lastMapPosition,
+          infoWindow: InfoWindow(
+            title: 'Really cool place',
+            snippet: '5 Star Rating',
+          ),
+          icon: BitmapDescriptor.defaultMarker,
+        )
+      );
+    });
+
+  }
+  _showQuantityModal(context){
+    final _formKey = GlobalKey<FormState>();
+    showModalBottomSheet(context: context, builder: (context)=> Container(
+                          color: Colors.red,
+                          child:Form(
+                            key: _formKey,
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: <Widget>[
+                                TextFormField(
+                                  decoration: const InputDecoration(
+                                    hintText: 'Enter your email',
+                                  ),
+                                  validator: (value) {
+                                    if (value.isEmpty) {
+                                      return 'Please enter some text';
+                                    }
+                                    return null;
+                                  },
+                                ),
+                                Padding(
+                                  padding: const EdgeInsets.symmetric(vertical: 16.0),
+                                  child: RaisedButton(
+                                    onPressed: () {
+                                      // Validate will return true if the form is valid, or false if
+                                      // the form is invalid.
+                                      if (_formKey.currentState.validate()) {
+                                        // Process data.
+                                      }
+                                    },
+                                    child: Text('Submit'),
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ));
   }
 
   // Reference https://alligator.io/flutter/geolocator-plugin/
