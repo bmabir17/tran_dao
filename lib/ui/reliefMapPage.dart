21a9a9cbc6d5d8221e99a84d9bc807df8ed6ca6a
bm.abir <bm.abir@chowagiken.co.jp>
2020-04-04 03:43:32 +0600

    UI Code cleanup

--- lib/ui/reliefMapPage.dart
+++ lib/ui/reliefMapPage.dart
@@ -18,15 +18,7 @@ class _ReliefMapPageState extends State<ReliefMapPage>{
   GoogleMapController mapController;
   static const LatLng _center = const LatLng(23.7805733,90.2792399);
   
-  final Set<Marker> _markers = {
-                  //Marker for current Location
-                  // Marker(
-                  //   markerId: MarkerId("marker"),
-                  //   position: LatLng(_currentPosition.latitude, _currentPosition.longitude),
-                  //   infoWindow: InfoWindow(title: 'Current Location'),
-                  //   icon: BitmapDescriptor.defaultMarkerWithHue(BitmapDescriptor.hueBlue)
-                  // )  
-              };
+  final Set<Marker> _markers = {};
   LatLng _lastMapPosition = _center;
   void _onMapCreated(GoogleMapController controller) {
     
@@ -53,13 +45,12 @@ class _ReliefMapPageState extends State<ReliefMapPage>{
               myLocationEnabled: true,
               myLocationButtonEnabled: true,
               initialCameraPosition: CameraPosition(
-                target:  LatLng(_currentPosition.latitude,_currentPosition.longitude) ,
+                target:  LatLng(_currentPosition.latitude,_currentPosition.longitude),
                 zoom: 11.0,
               ),
               markers: _markers,
               onCameraMove: _onCameraMove,
             ):
-            // Text("Loading Location"),
             Center(
               child: CircularProgressIndicator(),
             ),
@@ -71,9 +62,6 @@ class _ReliefMapPageState extends State<ReliefMapPage>{
                 child:FloatingActionButton.extended(
                   onPressed: () {
                     _showQuantityModal(context);
-                    // showModalBottomSheet(context: context, builder: (context)=> Container(
-                    //       color: Colors.red,
-                    //     ));
                   },
                   label: Text('New Relief'),
                   icon: Icon(Icons.add),
@@ -95,15 +83,13 @@ class _ReliefMapPageState extends State<ReliefMapPage>{
               child:Text("Loading Map"),
             ),
           ],
-        ),
-        
-        
+        ), 
       );
   }
 
   // Ref https://medium.com/@rajesh.muthyala/flutter-with-google-maps-and-google-place-85ccee3f0371
   void _onCameraMove(CameraPosition position) {
-  _lastMapPosition = position.target;
+    _lastMapPosition = position.target;
   }
   void _onAddMarkerButtonPressed(orgName,quantity,packageType) {
     quantity=quantity.toString();
@@ -121,103 +107,86 @@ class _ReliefMapPageState extends State<ReliefMapPage>{
         )
       );
     });
-
   }
   _showQuantityModal(context){
     final _formKey = GlobalKey<FormState>();
     int _quantity;
-    showModalBottomSheet(context: context, builder: (context)=> Container(
-                          color: Colors.red[50],
-                          height: 180,
-                          child:Stack(
-                            children: <Widget>[
-                              Form(
-                                key: _formKey,
-                                child: Column(
-                                  // crossAxisAlignment: CrossAxisAlignment.start,
-                                  children: <Widget>[
-                                    Padding(
-                                      padding: const EdgeInsets.all(10.0),
-                                      
-                                      child:Align(
-                                        alignment: Alignment.topCenter,
-                                        child: TextFormField(
-                                        decoration: const InputDecoration(
-                                          hintText: 'Enter your relief Quantity',
-                                          icon: Icon(Icons.add_box),
-                                          
-                                          ),
-                                          keyboardType: TextInputType.number,
-                                          inputFormatters: <TextInputFormatter>[
-                                              WhitelistingTextInputFormatter.digitsOnly
-                                          ],
-                                          validator: (value) {
-                                            if (value.isEmpty) {
-                                              return 'Please enter some text';
-                                            }
-                                            return null;
-                                          },
-                                          onChanged: (value){
-                                            // debugPrint("value: $value");
-                                            setState(() {
-                                              
-                                              _quantity = int.parse(value);
-                                            });
-                                          },
-                                          // onSaved: (value){
-                                          //   debugPrint("value: $value");
-                                          //   setState(() {
-                                              
-                                          //     _quantity = int.parse(value);
-                                          //   });
-                                          // },
-                                        ),
-                                      ),
-                                    ),
-                                    Padding(
-                                      padding: const EdgeInsets.symmetric(vertical: 25.0,horizontal: 140.0),
-                                      child:Align(
-                                        alignment: Alignment.center,
-                                        child: RaisedButton.icon(
-                                          icon: Icon(Icons.save, color:Colors.white,),
-                                          textColor: Colors.white,
-                                          splashColor: Colors.red,
-                                          color: Colors.green,
-                                          label: Text('Submit',style: TextStyle(color: Colors.white),),
-                                          shape: RoundedRectangleBorder(
-                                            
-                                            borderRadius: new BorderRadius.circular(10.0),
-                                            side: BorderSide(color: Colors.red[50])
-                                          ),
-                                          onPressed: () {
-                                            // Validate will return true if the form is valid, or false if
-                                            // the form is invalid.
-                                            if (_formKey.currentState.validate()) {
-                                              // Process data.
-                                              Navigator.pop(context);
-                                              debugPrint("_quantity: $_quantity");
-                                              _onAddMarkerButtonPressed("Abir",_quantity,"Weekly");
-
-                                            }
-                                          },
-                                          // child: Text('Submit'),
-                                        ),
-                                      ),
-                                    ),
-                                  ],
-                                ),
-                              ),
-                            ],
+    showModalBottomSheet(context: context, 
+      builder: (context)=> Container(
+        color: Colors.red[50],
+        height: 180,
+        child:Stack(
+          children: <Widget>[
+            Form(
+              key: _formKey,
+              child: Column(
+                children: <Widget>[
+                  Padding(
+                    padding: const EdgeInsets.all(10.0),
+                    child:Align(
+                      alignment: Alignment.topCenter,
+                      child: TextFormField(
+                        decoration: const InputDecoration(
+                            hintText: 'Enter your relief Quantity',
+                            icon: Icon(Icons.add_box),
                           ),
-                          
-                          
-                        ));
+                        keyboardType: TextInputType.number,
+                        inputFormatters: <TextInputFormatter>[
+                            WhitelistingTextInputFormatter.digitsOnly
+                        ],
+                        validator: (value) {
+                          if (value.isEmpty) {
+                            return 'Please enter some number';
+                          }
+                          return null;
+                        },
+                        onChanged: (value){
+                          setState(() {
+                            _quantity = int.parse(value);
+                          });
+                        },
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 25.0,horizontal: 140.0),
+                    child:Align(
+                      alignment: Alignment.center,
+                      child: RaisedButton.icon(
+                        icon: Icon(Icons.save, color:Colors.white),
+                        textColor: Colors.white,
+                        splashColor: Colors.red,
+                        color: Colors.green,
+                        label: Text('Submit',style: TextStyle(color: Colors.white)),
+                        shape: RoundedRectangleBorder( 
+                          borderRadius: new BorderRadius.circular(10.0),
+                          side: BorderSide(color: Colors.red[50])
+                        ),
+                        onPressed: () {
+                          // Validate will return true if the form is valid, or false if
+                          // the form is invalid.
+                          if (_formKey.currentState.validate()) {
+                            // Process data.
+                            Navigator.pop(context);
+                            debugPrint("_quantity: $_quantity");
+                            _onAddMarkerButtonPressed("Abir",_quantity,"Weekly");
+                          }
+                        },
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      )
+    );
   }
 
   // Reference https://alligator.io/flutter/geolocator-plugin/
    _getCurrentLocation () {
     final Geolocator geolocator = Geolocator()..forceAndroidLocationManager;
-
     geolocator
         .getCurrentPosition(desiredAccuracy: LocationAccuracy.best)
         .then((Position position) {
